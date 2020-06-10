a5869b77a9ad6a69d91c074914f1c13de636388b
JuicyLemo <istb-192_139296@volsu.ru>
2020-06-10 14:54:28 +0400

    Added a few lines

--- Лабораторная 8/main.c
+++ Лабораторная 8/main.c
@@ -1,6 +1,14 @@
+#include <stdio.h>
 #include "sdt.h"
 
 int main()
 {
-    
+    int a, b;
+
+    printf("\n¬ведите число a: ");
+    scanf("%d", &a);
+    printf("\n¬ведите число b: ");
+    scanf("%d", &b);
+
+    return 0;
 }
