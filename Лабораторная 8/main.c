05ece19f7011c8db75852b986328f2d699c0a588
JuicyLemo <istb-192_139296@volsu.ru>
2020-06-10 17:39:20 +0400

    Added subtraction

--- Лабораторная 8/main.c
+++ Лабораторная 8/main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <locale.h>
 #include "sdt.h"
 
 int main()
 {
+    setlocale(LC_ALL, "RUS");
     int a, b;
 
     printf("\n¬ведите число a: ");
     scanf("%d", &a);
     printf("\n¬ведите число b: ");
     scanf("%d", &b);
-    printf("%d + %d = %d", a, b, a+b);
+    printf("\n\n%d + %d = %d", a, b, a+b);
+    printf("\n%d - %d = %d", a, b, a-b);
     return 0;
 }
